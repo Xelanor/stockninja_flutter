daaf818fd7b1a083d8e0a211fa915d5c1706638f
Xelanor <44201710+Xelanor@users.noreply.github.com>
2020-02-13 20:56:57 +0300

    Search functionality

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -14,13 +14,38 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final _searchController = TextEditingController();
   var _isInit = true;
   var _isLoading = false;
   var _myStocks = [];
+  var _showedStocks = [];
 
   void addToMyStock(stockName) {
     const url = 'http://34.67.211.44/api/stock/add';
     http.post(url, body: {'name': stockName});
+  }
+
+  void filterSearchResults(String query) {
+    List dummySearchList = [];
+    dummySearchList.addAll(_myStocks);
+    if (query.isNotEmpty) {
+      List dummyListData = [];
+      dummySearchList.forEach((stock) {
+        if (stock['stockName'].toLowerCase().contains(query.toLowerCase())) {
+          dummyListData.add(stock);
+        }
+      });
+      setState(() {
+        _showedStocks.clear();
+        _showedStocks.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        _showedStocks.clear();
+        _showedStocks.addAll(_myStocks);
+      });
+    }
   }
 
   void getMyStocks() {
@@ -34,6 +59,7 @@ class _SearchScreenState extends State<SearchScreen> {
         setState(() {
           _isLoading = false;
           _myStocks = extractedData.sublist(0, extractedData.length - 2);
+          _showedStocks = extractedData.sublist(0, extractedData.length - 2);
         });
       },
     ).catchError((err) {
@@ -60,22 +86,48 @@ class _SearchScreenState extends State<SearchScreen> {
             child: CircularProgressIndicator(
                 valueColor:
                     AlwaysStoppedAnimation(Theme.of(context).primaryColor)))
-        : Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              StockTableHeader(),
-              Expanded(
-                child: RefreshIndicator(
-                  onRefresh: refresh,
-                  child: ListView.builder(
-                    physics: BouncingScrollPhysics(),
-                    itemCount: _myStocks.length,
-                    itemBuilder: (_, i) =>
-                        StockTableSearchRow(_myStocks[i], addToMyStock),
+        : GestureDetector(
+            onTap: () {
+              FocusScope.of(context).requestFocus(new FocusNode());
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                StockTableHeader(),
+                Container(
+                  margin: EdgeInsets.only(left: 5, right: 5, bottom: 5),
+                  height: 40,
+                  child: TextField(
+                    onChanged: (value) {
+                      filterSearchResults(value);
+                    },
+                    textCapitalization: TextCapitalization.characters,
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                        fillColor: Theme.of(context).colorScheme.background,
+                        filled: true,
+                        contentPadding: EdgeInsets.all(0),
+                        labelText: "Search",
+                        hintText: "Search",
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)))),
                   ),
                 ),
-              ),
-            ],
+                Expanded(
+                  child: RefreshIndicator(
+                    onRefresh: refresh,
+                    child: ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      itemCount: _showedStocks.length,
+                      itemBuilder: (_, i) =>
+                          StockTableSearchRow(_showedStocks[i], addToMyStock),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           );
   }
 
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -26,7 +26,7 @@ class _TabsScreenState extends State<TabsScreen> {
     },
     {
       'page': WillPopScope(child: SearchScreen(), onWillPop: () {}),
-      'title': 'Search'
+      'title': 'Tüm Hisseler'
     },
     {
       'page': WillPopScope(child: SettingsScreen(), onWillPop: () {}),
@@ -44,50 +44,55 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        title: Text(
-          _pages[_selectedPageIndex]['title'],
-          style: TextStyle(
-              color: Theme.of(context).colorScheme.onBackground,
-              fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(
+          title: Text(
+            _pages[_selectedPageIndex]['title'],
+            style: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground,
+                fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          brightness: Theme.of(context).brightness,
         ),
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        brightness: Theme.of(context).brightness,
-      ),
-      body: _pages[_selectedPageIndex]['page'],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        onTap: _selectPage,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        unselectedItemColor: Colors.white,
-        selectedItemColor: Theme.of(context).colorScheme.primary,
-        currentIndex: _selectedPageIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.attach_money),
-            title: Text('Homepage'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_balance_wallet),
-            title: Text('Events'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.blur_circular),
-            title: Text('Favorites'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title: Text('Search'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text('Settings'),
-          )
-        ],
+        body: _pages[_selectedPageIndex]['page'],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: _selectPage,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).colorScheme.primary,
+          currentIndex: _selectedPageIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.attach_money),
+              title: Text('Homepage'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance_wallet),
+              title: Text('Events'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.blur_circular),
+              title: Text('Favorites'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text('Search'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('Settings'),
+            )
+          ],
+        ),
       ),
     );
   }
